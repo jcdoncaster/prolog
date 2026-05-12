24e8e4932aa8dbcaf216394c3fc6e77ba814be7c
jcdoncaster <atifahnafchw@gmail.com>
2026-05-13 00:12:58 +0600

    Auto-sync: math.pl at 00:12:58

--- Desktop/prolog/math.pl
+++ Desktop/prolog/math.pl
@@ -1,6 +1,20 @@
-addition(X , Y):- Z is X + Y, write(Z).
-subtraction(X , Y):- Z is X - Y, write(Z).
-multiplication(X , Y):- Z is X * Y, write(Z).
-division(X , Y):- Z is X / Y, write(Z).
-is_even(X) :- X mod 2 =:= 0.
-is_odd(X) :- X mod 2 =\= 0.
+/* A few things have happened in a very short time, i learnt propositional logic then
+moved to predicate logic and somehow after all these steps, we are getting into 
+PROLOG, thats so hidious that i dont know how to say that i myself am not really confident in
+it. idk how my ancestors worked in it." - A confused student */
+
+% Facts
+number(1:10).
+% Rules
+even(X) :- number(X), X mod 2 =:= 0.
+odd(X) :- number(X), X mod 2 =:= 1.
+greater_than(X, Y) :- number(X), number(Y), X > Y.
+% Queries
+% To find all even numbers:
+% ?- even(X).
+% To find all odd numbers:
+% ?- odd(X).
+% To find all pairs of numbers where the first is greater than the second:
+% ?- greater_than(X, Y).
+
+%easy innit?
